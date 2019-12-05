5fd6073966d2d4b054c8aab76cb6bdb6507470e1
Imre Kristoffer Eilertsen <imreeil42@gmail.com>
2019-12-05 14:32:42 +0100

    This took an obscene amount of time to successfully craft

--- AdGuard Home Compilation List/AGHtest.py
+++ AdGuard Home Compilation List/AGHtest.py
@@ -1,12 +1,14 @@
 import requests
 import re
 
-SOURCES = ['https://easylist-downloads.adblockplus.org/easylist_noelemhide.txt', 'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters.txt', 'https://raw.githubusercontent.com/NanoAdblocker/NanoFilters/master/NanoMirror/NanoDefender.txt', 'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/badware.txt', 'https://easylist-downloads.adblockplus.org/liste_fr.txt', 'https://www.i-dont-care-about-cookies.eu/abp/', 'https://easylist-downloads.adblockplus.org/easylistgermany.txt', 'https://easylist-downloads.adblockplus.org/abp-filters-anti-cv.txt']
+SOURCES = ['https://raw.githubusercontent.com/DandelionSprout/adfilt/master/AdGuard%20Home%20Compilation%20List/TopDescription.notlist', 'https://easylist-downloads.adblockplus.org/easylist_noelemhide.txt', 'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters.txt', 'https://raw.githubusercontent.com/NanoAdblocker/NanoFilters/master/NanoMirror/NanoDefender.txt', 'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/badware.txt', 'https://easylist-downloads.adblockplus.org/liste_fr.txt', 'https://www.i-dont-care-about-cookies.eu/abp/', 'https://easylist-downloads.adblockplus.org/easylistgermany.txt', 'https://easylist-downloads.adblockplus.org/abp-filters-anti-cv.txt']
 
-UNSUPPORTED_AGH = ['/', '##', '@#', '#?#', 'domain=', 'generichide', '$csp', 'badfilter', 'xmlhttprequest', '$xhr', '$stylesheet', '~image', '$elemhide', '$inline-script', '$other', '$~object', 'redirect=', '#$#']
+UNSUPPORTED_AGH = ['##', '@#', '#?#', 'domain=', 'generichide', '$csp', 'badfilter', 'xmlhttprequest', '$xhr', '$stylesheet', '~image', '$elemhide', '$inline-script', '$other', '$~object', 'redirect=', '#$#']
+UNSUPPORTED_IP = ['##', '@#', '#?#', 'domain=', 'generichide', '$csp', 'badfilter', 'xmlhttprequest', '$xhr', '$stylesheet', '~image', '$elemhide', '$inline-script', '$other', '$~object', 'redirect=', '#$#']
 
 OUTPUT = 'xyzzyx.txt'
 OUTPUT_AGH = 'AdGuardHomeCompilationList.txt'
+OUTPUT_IP = 'AdGuardHomeCompilationListIPs.txt'
 
 # function that downloads the filter list
 def download_filters() -> str:
@@ -190,7 +192,350 @@ def prepare_agh(lines) -> str:
            line
         )
 
+        line = re.sub(
+           r"^Amasty_.*", 
+           "", 
+           line
+        )
+
+        line = re.sub(
+           r"^\*.*", 
+           "", 
+           line
+        )
+
+        line = re.sub(
+           r"^\\.*", 
+           "", 
+           line
+        )
+
+        line = re.sub(
+           r"^:.*", 
+           "", 
+           line
+        )
+
+        line = re.sub(
+           r"^@[a-z0-9].*", 
+           "", 
+           line
+        )
+
+        line = re.sub(
+           r"^[/\\].*", 
+           r"", 
+           line
+        )
+
+        line = re.sub(
+           r"@?@?\|\|.*/.*", 
+           r"", 
+           line
+        )
+
         if is_supported_agh(line) and not line == '':
+            text += line + '\r\n'
+
+    return text
+
+# ——— IP version ———
+
+def is_supported_ip(line) -> bool:
+    for token in UNSUPPORTED_IP:
+        if token in line:
+            return False
+
+    return True
+
+    # function that prepares the filter list for AdGuard Home
+def prepare_ip(lines) -> str:
+    text = ''
+
+    previous_line = None
+
+    for line in lines:
+            
+        if line == previous_line:
+            continue
+
+        line = re.sub(
+           r"([$,])third-party", 
+           "", 
+           line
+        )
+
+        line = re.sub(
+           r"([$,])~third-party", 
+           "", 
+           line
+        )
+
+        line = re.sub(
+           r"([$,])3p", 
+           "", 
+           line
+        )
+
+        line = re.sub(
+           r"([$,])first-party", 
+           "", 
+           line
+        )
+
+        line = re.sub(
+           r"([$,])1p", 
+           "", 
+           line
+        )
+
+        line = re.sub(
+           r"([$,])image", 
+           "", 
+           line
+        )
+
+        line = re.sub(
+           r"([$,])media", 
+           "", 
+           line
+        )
+
+        line = re.sub(
+           r"([$,])script", 
+           "", 
+           line
+        )
+
+        line = re.sub(
+           r"([$,])popup", 
+           "", 
+           line
+        )
+
+        line = re.sub(
+           r"([$,])popunder", 
+           "", 
+           line
+        )
+
+        line = re.sub(
+           r"([$,])document", 
+           "", 
+           line
+        )
+
+        line = re.sub(
+           r"([$,])subdocument", 
+           "", 
+           line
+        )
+
+        line = re.sub(
+           r"([$,])~subdocument", 
+           "", 
+           line
+        )
+
+        line = re.sub(
+           r"([$,])object", 
+           "", 
+           line
+        )
+
+        line = re.sub(
+           r"([$,])~object-subrequest", 
+           "", 
+           line
+        )
+
+        line = re.sub(
+           r"([$,])frame", 
+           "", 
+           line
+        )
+
+        line = re.sub(
+           r"([$,])all", 
+           "", 
+           line
+        )
+
+        line = re.sub(
+           r",important", 
+           "$important", 
+           line
+        )
+
+        line = re.sub(
+           r".*\^[*$,].*", 
+           "", 
+           line
+        )
+
+        line = re.sub(
+           r".*\.js$", 
+           "", 
+           line
+        )
+
+        line = re.sub(
+           r"^[&+-.=?^_%].*", 
+           "", 
+           line
+        )
+
+        line = re.sub(
+           r"^([a-z])", 
+           r"||\1", 
+           line
+        )
+
+        line = re.sub(
+           r"^[0-9].*", 
+           "", 
+           line
+        )
+
+        line = re.sub(
+           r"^\|[a-z0-9].*", 
+           "", 
+           line
+        )
+
+        line = re.sub(
+           r"^;[a-z0-9].*", 
+           "", 
+           line
+        )
+
+        line = re.sub(
+           r"^Amasty_.*", 
+           "", 
+           line
+        )
+
+        line = re.sub(
+           r"^\*.*", 
+           r"", 
+           line
+        )
+
+        line = re.sub(
+           r"^\\.*", 
+           r"", 
+           line
+        )
+
+        line = re.sub(
+           r"^:.*", 
+           r"", 
+           line
+        )
+
+        line = re.sub(
+           r"^@[a-z0-9].*", 
+           r"", 
+           line
+        )
+
+        line = re.sub(
+           r"[@]?[@]?\|\|.*[a-zみ].*", 
+           r"", 
+           line
+        )
+
+        line = re.sub(
+           r"! (Version: .*[a-z].*)", 
+           r"# \1", 
+           line
+        )
+
+        line = re.sub(
+           r"^![a-z #].*", 
+           r"", 
+           line
+        )
+
+        line = re.sub(
+           r"^!-.*", 
+           r"", 
+           line
+        )
+
+        line = re.sub(
+           r"^!$", 
+           r"", 
+           line
+        )
+
+        line = re.sub(
+           r"\[Adblock Plus 2\.0\]", 
+           r"", 
+           line
+        )
+
+        line = re.sub(
+           r"\[Nano Adblocker\]", 
+           r"", 
+           line
+        )
+
+        line = re.sub(
+           r"\|\|", 
+           r"", 
+           line
+        )
+
+        line = re.sub(
+           r"\^$", 
+           r"", 
+           line
+        )
+
+        line = re.sub(
+           r"^([0-9][0-9]?[0-9]?\.[0-9][0-9]?[0-9]?\.[0-9][0-9]?[0-9]?)\.$", 
+           r"\1.0/24", 
+           line
+        )
+
+        line = re.sub(
+           r"^([0-9][0-9]?[0-9]?\.[0-9][0-9]?[0-9]?)\.$", 
+           r"\1.0.0/16", 
+           line
+        )
+
+        line = re.sub(
+           r"^[/\\].*", 
+           r"", 
+           line
+        )
+
+        line = re.sub(
+           r"^@@.*[a-z].*", 
+           r"", 
+           line
+        )
+
+        line = re.sub(
+           r"\[Adblock Plus 3\.4\]", 
+           "! Title: IP Entries from Adblock Lists\n! Contains transformated entries from: EasyList, uBlock Filters, uBlock Filters - Badware Risks, Nano Defender Integration, Liste FR, I Don't Care About Cookies, EasyList Germany, ABP Anti-Circumvention Filters\n! Expires: 14 days\n! Licence: In accordance with the Dandelicence, the borrowed entries are considered to have been changed and reduced enough from their original lists, that they're counted as transformative work, meaning that creditation and seperate paragraphs are not necessary unless one of the lists' makers were to ask for such.\n! Description: This was made as a proof-of-concept to see if the IP-based entries of major adblock lists, could be used to create an IP adblocker list for IP blockers, despite how IP lists are normally only meant to block malware, E-mail spam, or port scanners.", 
+           line
+        )
+
+        line = re.sub(
+           r"# Version: (.*)-.*", 
+           r"! Version: \1-Alpha", 
+           line
+        )
+
+        line = re.sub(
+           r"^!$", 
+           r"", 
+           line
+        )
+
+        if is_supported_ip(line) and not line == '':
             text += line + '\r\n'
 
     return text
@@ -202,6 +547,7 @@ if __name__ == "__main__":
     print('Total number of rules: ' + str(len(lines)))
 
     agh_filter = prepare_agh(lines)
+    ip_filter = prepare_ip(lines)
 
     with open(OUTPUT, "w") as text_file:
         text_file.write(text)
@@ -209,4 +555,7 @@ if __name__ == "__main__":
     with open(OUTPUT_AGH, "w") as text_file:
         text_file.write(agh_filter)
 
-    print('The domains-based list versions have been generated.')
+    with open(OUTPUT_IP, "w") as text_file:
+        text_file.write(ip_filter)
+
+    print('The list versions have been generated.')
