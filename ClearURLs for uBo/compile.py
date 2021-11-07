4097d93de1dcba5b4c608e405f2fa51d37acecc0
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-11-07 06:58:30 -0500

    Some minor changes

--- ClearURLs for uBo/compile.py
+++ ClearURLs for uBo/compile.py
@@ -31,7 +31,8 @@ HEAD = """\
 ! Title: ClearURLs for uBo - List extension
 ! Homepage: https://github.com/DandelionSprout/adfilt/discussions/163
 ! Last updated: {date}
-! Note: This was forked from https://gist.github.com/rusty-snake/5cd83a87d680ecbd03e79a1a06758207, which is based off of https://github.com/ClearURLs/Rules. The maintainers of Adfilt (DandelionSprout and iam-py-test, and contributors) have made some modifications as to keep it up-to-date with the source and to fix issues
+! Expires: 1 day
+! Note: This was based off of https://gist.github.com/rusty-snake/5cd83a87d680ecbd03e79a1a06758207, which is based off of https://github.com/ClearURLs/Rules. The maintainers of Adfilt (DandelionSprout and iam-py-test, and contributors) have made some modifications as to keep it up-to-date with the source and to fix issues
 
 """
 
