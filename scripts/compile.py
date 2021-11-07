3734c98fadb6e9496432101dea0e604412984937
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-11-07 11:30:51 +0000

    Add the head & date
    Co-authored-by: rusty-snake <41237666+rusty-snake@users.noreply.github.com>

--- scripts/compile.py
+++ scripts/compile.py
@@ -115,6 +115,7 @@ def main() -> int:
     global endrules
     data_min_json = json.loads(getrules())
     endrules = open("uBO list extensions/clear_urls_uboified.txt", "w")
+    endrules.write(HEAD.format(date=date.today().strftime("%d/%m/%Y")))
 
     # TODO: referralMarketing
     providers = {
