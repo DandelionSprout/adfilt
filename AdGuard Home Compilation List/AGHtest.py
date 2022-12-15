f5e415e8f53ac3cf6adf2dc669d036843a4f5261
Imre Kristoffer Eilertsen <imreeil42@gmail.com>
2022-12-15 10:51:57 +0100

    Add files via upload
    Fine tuning is complicated

--- AdGuard Home Compilation List/AGHtest.py
+++ AdGuard Home Compilation List/AGHtest.py
@@ -938,6 +938,12 @@ def prepare_agh(lines) -> str:
            line
         )
 
+        line = re.sub(
+           r"^(:/)?/[b-z]([a-z]{1,2})?\.$", 
+           r"", 
+           line
+        )
+
         if is_supported_agh(line) and not line == '':
             text += line + '\r\n'
 
