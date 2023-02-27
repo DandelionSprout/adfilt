d04600d64f84544fd6b1d62309e268c22d90243e
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2023-02-26 19:58:25 -0500

    Fix https://github.com/DandelionSprout/adfilt/discussions/163#discussioncomment-5017320

--- ClearURLs for uBo/compile.py
+++ ClearURLs for uBo/compile.py
@@ -34,7 +34,7 @@ HEAD = """\
 ! Homepage: https://github.com/DandelionSprout/adfilt/discussions/163
 ! Description: Want to use ClearURLs' tracking protection without installing another extension? This list is a (unofficial) version of the ClearURLs rules, designed for use in uBlock Origin and AdGuard. This ONLY includes the URL parameter removal functionality from ClearURLs, and not the other functions.
 ! Last updated: {date}
-! Script last updated: 13/2/2023
+! Script last updated: 26/2/2023
 ! Expires: 1 day
 ! Licence: https://github.com/DandelionSprout/adfilt/blob/master/LICENSE.md
 ! Warning: This list may break websites, and contains many problematic rules. There is not much the Adfilt maintainers can do, as this list is just the ClearURLs rules converted into a uBo/AdGuard filterlist. Use with caution.
@@ -88,6 +88,9 @@ KNOWN_BAD_FILTERS = [
     "||amazon.*/s?$removeparam=rnid",
     # breaks GSB lookup - https://github.com/DandelionSprout/adfilt/discussions/163#discussioncomment-4337362
     "||google.*^$removeparam=site",
+    # https://github.com/DandelionSprout/adfilt/discussions/163#discussioncomment-5017320
+    "||firefox.com^$removeparam=entrypoint",
+    "||firefox.com^$removeparam=context",
 ]
 
 ALLOWLIST = """
