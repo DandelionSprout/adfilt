879fdfaebe470988bbd877c69a3499a62e72e19c
Imre Eilertsen <imreeil42@gmail.com>
2024-05-25 13:38:24 +0200

    Add files via upload
    I got a fairly good night of sleep and thus was again able to concentrate.

--- NorwegianExperimentalList alternate versions/XYZPrepareFilters.py
+++ NorwegianExperimentalList alternate versions/XYZPrepareFilters.py
@@ -3253,7 +3253,7 @@ def prepare_abp(lines) -> str:
 
         line = re.sub(
            r"^(! If you wish to remove.*)", 
-           r"!•\n\1\n!•\n! If you see “/v3/full/“ in the list's URL or a “! DiffUrl:“ row above, while you're using Firefox, ABP 3.x, or a non-ABP extension, then you have been scammed and must change the list subscription to https://easylist-downloads.adblockplus.org/dandelion_sprouts_nordic_filters+easylist.txt immediately.\n!•", 
+           r"!•\n\1\n!•\n! If you see “/v3/full/“ in the list's URL or a “! DiffUrl:“ row above, while you're using Firefox, ABP 3.x, or a non-ABP extension, then you have been scammed and must change the list subscription to https://easylist-downloads.adblockplus.org/dandelion_sprouts_nordic_filters+easylist.txt immediately. If you use ABP 4.x in Chrome, there is no reason to worry.\n!•", 
            line
         )
 
